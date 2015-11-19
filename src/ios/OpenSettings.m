c9da3fa6ae49ed93f725a58543c6cf7813aaf44a
Marcel Ball <me@marcelball.ca>
2015-11-18 22:58:16 -0400

    Stripped out BlueTooth Functionality

--- src/ios/OpenSettings.m
+++ src/ios/OpenSettings.m
@@ -7,7 +7,6 @@
 
 @implementation OpenSettings
 
-
 -(void) settings:(CDVInvokedUrlCommand*)command {
 
     CDVPluginResult* pluginResult = nil;
@@ -18,47 +17,6 @@
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
--(void) bluetooth:(CDVInvokedUrlCommand*)command {
-
-    CDVPluginResult* pluginResult = nil;
-
-    [[CBCentralManager alloc] initWithDelegate:self queue:nil];
-
-    // TODO check if is iOS8 otherwise error
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
--(void) bluetoothStatus:(CDVInvokedUrlCommand*)command {
-
-  CDVPluginResult* pluginResult = nil;
-  NSString *stateName = [self peripherialStateAsString:_peripheralManager.state];
-
-  NSLog(@"Current bt status: %@", stateName);
-  NSDictionary *dict = @{@"status": stateName};
-
-  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dict];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
-}
-
-- (void)pluginInitialize {
-    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
-}
-
-- (void) peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral { }
-
-- (NSString*) peripherialStateAsString: (CBPeripheralManagerState) state {
-    NSDictionary *dict = @{@(CBPeripheralManagerStatePoweredOff): @"PeripheralManagerStatePoweredOff",
-                           @(CBPeripheralManagerStatePoweredOn): @"PeripheralManagerStatePoweredOn",
-                           @(CBPeripheralManagerStateResetting): @"PeripheralManagerStateResetting",
-                           @(CBPeripheralManagerStateUnauthorized): @"PeripheralManagerStateUnauthorized",
-                           @(CBPeripheralManagerStateUnknown): @"PeripheralManagerStateUnknown",
-                           @(CBPeripheralManagerStateUnsupported): @"PeripheralManagerStateUnsupported"};
-    return [dict objectForKey:[NSNumber numberWithInteger:state]];
 }
 
 @end
